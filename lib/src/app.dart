184850d2f5b6ce7d237333974b29bff905c7940f
Robin <daorob@gmail.com>
2020-01-14 02:10:38 -0600

    Convert app from a StatelessWidget to a StatefulWidget.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,18 +1,29 @@
 // import flutter helper library
 import 'package:flutter/material.dart';
 
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
 // create a class that will be our custom widget
 // this class must extend the 'StatelessWidget' base class
 // App is now a subclass of Statelesswidget
-class App extends StatelessWidget {
+class AppState extends State<App> {
+  int counter = 0;
+
   // Must define a 'build' method that returns the widgets that *this* widget will show
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
+        body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
-            print('Hi there!');
+            setState(() {
+              counter += 1;
+            });
           },
         ),
         appBar: AppBar(
