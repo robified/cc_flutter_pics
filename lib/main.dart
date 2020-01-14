34b620b232835b4856c48f227d0da320cb80f6dc
Robin <daorob@gmail.com>
2020-01-14 01:12:25 -0600

    Add button to screen. Press button for print fn working.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Hi there!');
+        },
+      ),
       appBar: AppBar(
         title: Text('Lets see some images!'),
       ),
