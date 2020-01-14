504f9c1d2f90c362d92f6ea914a428ee4b54b2bf
Robin <daorob@gmail.com>
2020-01-14 01:39:09 -0600

    Refactor main to app.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,4 +6,19 @@ import 'package:flutter/material.dart';
 // App is now a subclass of Statelesswidget
 class App extends StatelessWidget {
   // Must define a 'build' method that returns the widgets that *this* widget will show
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            print('Hi there!');
+          },
+        ),
+        appBar: AppBar(
+          title: Text('Lets see some images!'),
+        ),
+      ),
+    );
+  }
 }
