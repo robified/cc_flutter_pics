ea353c9d7271a031f5a35805be671818150b1555
Robin <daorob@gmail.com>
2020-01-14 00:33:29 -0600

    Add main function.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 
 // Define a  'main' function to run when our app starts
-
-// Create a new text widget to show some text on the screen
-
-// Take that widget and get it on the screen
+void main() {
+  // Create a new text widget to show some text on the screen
+  // Take that widget and get it on the screen
+}
