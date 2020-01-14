b3d0e78e9fab68563e12b16429654b32cf2555d1
Robin <daorob@gmail.com>
2020-01-14 00:38:31 -0600

    Add 'herro there' to Text widget.

--- lib/main.dart
+++ lib/main.dart
@@ -5,5 +5,9 @@ import 'package:flutter/material.dart';
 // Define a  'main' function to run when our app starts
 void main() {
   // Create a new text widget to show some text on the screen
+  var app = MaterialApp(
+    home: Text('Herro there!'),
+  );
+
   // Take that widget and get it on the screen
 }
