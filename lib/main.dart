dd6635842e5e7393f410ed8ace8bafb6cda604e9
Robin <daorob@gmail.com>
2020-01-14 01:00:35 -0600

    Add a customized appBar widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
-// Import a helper library from Flutter to get content on the screen
-// import code from a 3rd party package
 import 'package:flutter/material.dart';
 
-// Define a  'main' function to run when our app starts
 void main() {
-  // Create a new text widget to show some text on the screen
   var app = MaterialApp(
-    home: Text('Herro there!'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Lets see some images!'),
+      ),
+    ),
   );
 
-  // Take that widget and get it on the screen
   runApp(app);
 }
