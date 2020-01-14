74863639b878fdd4488ad599fa9daeb2212d467c
Robin <daorob@gmail.com>
2020-01-14 00:32:10 -0600

    Import flutter package material file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,9 @@
+// Import a helper library from Flutter to get content on the screen
+// import code from a 3rd party package
+import 'package:flutter/material.dart';
 
+// Define a  'main' function to run when our app starts
+
+// Create a new text widget to show some text on the screen
+
+// Take that widget and get it on the screen
