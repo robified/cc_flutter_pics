988d0aeab5f4850aaa7f6c8d27dfc764ab53dad6
Robin <daorob@gmail.com>
2020-01-14 05:07:25 -0600

    Create an ImageList class and implement the ListView widget to return URLs texts.

---
+++ lib/src/widgets/image_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../models/image_model.dart';
+
+class ImageList extends StatelessWidget {
+  final List<ImageModel> images;
+
+  ImageList(this.images);
+
+  Widget build(context) {
+    return ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (context, int index) {
+        return Text(images[index].url);
+      },
+    );
+  }
+}
